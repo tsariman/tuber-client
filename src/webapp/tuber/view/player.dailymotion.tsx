a0713cdea41b4972346438c5c8f89490effeaf98
crownlessking <riviere.king@outlook.com>
2026-01-02 00:14:03 -0500

    [Refactor] Removed unnecessary comments

--- src/webapp/tuber/view/player.dailymotion.tsx
+++ src/webapp/tuber/view/player.dailymotion.tsx
@@ -1,18 +1,16 @@
-import { styled } from '@mui/material/styles';
-import React, { useMemo } from 'react';
-import type { IBookmark } from '../tuber.interfaces';
+import { styled } from '@mui/material/styles'
+import React, { useMemo } from 'react'
+import type { IBookmark } from '../tuber.interfaces'
 
 interface IDailyPlayerProps {
-  bookmark: IBookmark;
+  bookmark: IBookmark
 }
 
 const StyledIframeDiv = styled('div')(() => ({
-  // paddingBottom: '56.25%',
-  // overflow: 'hidden',
   position: 'relative',
   width: '100%',
   height: '100%',
-}));
+}))
 
 const IframeStyled = styled('iframe')(() => ({
   position: 'absolute',
@@ -21,22 +19,22 @@ const IframeStyled = styled('iframe')(() => ({
   overflow: 'hidden',
   width: '100%',
   height: '100%',
-}));
+}))
 
 /**
  * [TODO] You have to let user enter the start time in seconds.
  */
 const DailyPlayer = React.memo<IDailyPlayerProps>(({ bookmark }) => {
-  const { videoid, start_seconds } = bookmark;
+  const { videoid, start_seconds } = bookmark
   
   // Memoize start time calculation
-  const start = useMemo(() => start_seconds ?? 0, [start_seconds]);
+  const start = useMemo(() => start_seconds ?? 0, [start_seconds])
   
   // Memoize src URL to prevent iframe recreation
   const src = useMemo(() => 
     `https://www.dailymotion.com/embed/video/${videoid}?autoplay=1&start=${start}`,
     [videoid, start]
-  );
+  )
 
   return (
     <StyledIframeDiv>
@@ -50,10 +48,10 @@ const DailyPlayer = React.memo<IDailyPlayerProps>(({ bookmark }) => {
         allowFullScreen
       />
     </StyledIframeDiv>
-  );
-});
+  )
+})
 
 // Set display name for debugging
-DailyPlayer.displayName = 'DailyPlayer';
+DailyPlayer.displayName = 'DailyPlayer'
 
-export default DailyPlayer;
+export default DailyPlayer
--- src/webapp/tuber/view/player.odysee.tsx
+++ src/webapp/tuber/view/player.odysee.tsx
@@ -1,18 +1,16 @@
-import { styled } from '@mui/material/styles';
-import React, { useMemo } from 'react';
-import type { IBookmark } from '../tuber.interfaces';
+import { styled } from '@mui/material/styles'
+import React, { useMemo } from 'react'
+import type { IBookmark } from '../tuber.interfaces'
 
 interface IOdyseePlayerProps {
-  bookmark: IBookmark;
+  bookmark: IBookmark
 }
 
 const StyledIframeDiv = styled('div')(() => ({
-  // paddingBottom: '56.25%',
-  // overflow: 'hidden',
   position: 'relative',
   width: '100%',
   height: '100%',
-}));
+}))
 
 const IframeStyled = styled('iframe')(() => ({
   position: 'absolute',
@@ -21,22 +19,22 @@ const IframeStyled = styled('iframe')(() => ({
   overflow: 'hidden',
   width: '100%',
   height: '100%',
-}));
+}))
 
 /**
  * [TODO] You have to let user enter the start time in seconds.
  */
 const OdyseePlayer = React.memo<IOdyseePlayerProps>(({ bookmark }) => {
-  const { slug, start_seconds } = bookmark;
+  const { slug, start_seconds } = bookmark
   
   // Memoize start time calculation
-  const start = useMemo(() => start_seconds ?? 0, [start_seconds]);
+  const start = useMemo(() => start_seconds ?? 0, [start_seconds])
   
   // Memoize src URL to prevent iframe recreation
   const src = useMemo(() => 
     `https://odysee.com/$/embed/${slug}?t=${start}&autoplay=true`,
     [slug, start]
-  );
+  )
 
   return (
     <StyledIframeDiv>
@@ -50,10 +48,10 @@ const OdyseePlayer = React.memo<IOdyseePlayerProps>(({ bookmark }) => {
         allowFullScreen
       />
     </StyledIframeDiv>
-  );
-});
+  )
+})
 
 // Set display name for debugging
-OdyseePlayer.displayName = 'OdyseePlayer';
+OdyseePlayer.displayName = 'OdyseePlayer'
 
-export default OdyseePlayer;
+export default OdyseePlayer
